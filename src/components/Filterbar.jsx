aa59c936ca46e1ccf75521b21175f8f2d43b65de
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-11-15 20:33:33 +0100

    production build fixes

--- src/components/Filterbar.jsx
+++ src/components/Filterbar.jsx
@@ -8,10 +8,7 @@ export default function Filterbar({ filters, removeFilter }) {
           <li key={filter}>
             <span>{filter}</span>
             <button onClick={() => removeFilter(filter)}>
-              <img
-                src="./src/assets/images/icon-remove.svg"
-                alt="remove filter"
-              />
+              <img src="./public/images/icon-remove.svg" alt="remove filter" />
             </button>
           </li>
         ))}
