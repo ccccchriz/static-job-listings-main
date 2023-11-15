5f925640cfafedfda09d53d71e025d0e9280ac47
ccccchriz <76066995+ccccchriz@users.noreply.github.com>
2023-11-15 19:18:29 +0100

    Styled filterbar

--- src/components/Filterbar.jsx
+++ src/components/Filterbar.jsx
@@ -3,19 +3,23 @@ import "./Filterbar.css";
 export default function Filterbar({ filters, removeFilter }) {
   return (
     <section className="filterbar">
-      {filters.map((filter) => (
-        <li key={filter}>
-          <span>{filter}</span>
-          <button>
-            <img
-              onClick={() => removeFilter(filter)}
-              src="./src/assets/images/icon-remove.svg"
-              alt="remove filter"
-            />
-          </button>
-        </li>
-      ))}
-      <button onClick={() => removeFilter()}>Clear</button>
+      <ul className="filterbar__list">
+        {filters.map((filter) => (
+          <li key={filter}>
+            <span>{filter}</span>
+            <button>
+              <img
+                onClick={() => removeFilter(filter)}
+                src="./src/assets/images/icon-remove.svg"
+                alt="remove filter"
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button className="filterbar__clear" onClick={() => removeFilter()}>
+        Clear
+      </button>
     </section>
   );
 }
